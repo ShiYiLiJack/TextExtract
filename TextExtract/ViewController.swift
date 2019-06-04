372432aad58a9394fc11177ed2cd758ef6c3626c
Jack <jack@Jacks-MacBook-Pro.local>
2019-06-04 14:16:28 -0700

    first commit

---
+++ TextExtract/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextExtract
+//
+//  Created by Jack on 6/4/19.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
