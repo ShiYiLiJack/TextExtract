ffec24b3dfc9001e7164f946a3ce2abab130bf53
Jack <jack@Jacks-MacBook-Pro.local>
2019-06-06 18:06:54 -0700

    added imagepicker

--- Podfile
+++ Podfile
@@ -5,6 +5,11 @@ target 'TextExtract' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for TextExtract
+    # Pods for TextExtract
     pod 'Firebase/Core'
+	
+    pod 'Firebase/Analytics'
+    pod 'Firebase/MLVision'
+    # If using an on-device API:
+    pod 'Firebase/MLVisionTextModel'
 end
--- TextExtract/ViewController.swift
+++ TextExtract/ViewController.swift
@@ -7,20 +7,35 @@
 //
 
 import UIKit
+import FirebaseMLVision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate {
     var titles: String?
     var text: String?
     
+    let cameraImage = UIImagePickerController()
+    let imagePicked = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("")
+        }
+        
+        
+    }
+    
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(cameraImage, animated: true, completion: nil)
     }
     
     @IBAction func fileButtonPressed(_ sender: UIBarButtonItem) {
+        present(imagePicked, animated: true, completion: nil)
     }
 }
 
